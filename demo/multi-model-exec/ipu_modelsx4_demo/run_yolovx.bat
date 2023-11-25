5fefd1c3c305c9106d3ac7d53771dd56df12f997
lidachang-amd <deandre880119@gmail.com>
2023-11-26 01:23:15 +0800

    update arc3

--- demo/multi-model-exec/ipu_modelsx4_demo/run_yolovx.bat
+++
@@ -1,8 +0,0 @@
-set XLNX_VART_FIRMWARE=%cd%\..\1x4.xclbin
-set PATH=%cd%\..\bin;%cd%\..\python;%cd%\..;%PATH%
-set DEBUG_ONNX_TASK=0
-set DEBUG_DEMO=0
-set NUM_OF_DPU_RUNNERS=4
-set XLNX_ENABLE_GRAPH_ENGINE_PAD=1
-set XLNX_ENABLE_GRAPH_ENGINE_DEPAD=1
-%cd%\..\bin\ipu_multi_models.exe %cd%\config\yolovx.json
