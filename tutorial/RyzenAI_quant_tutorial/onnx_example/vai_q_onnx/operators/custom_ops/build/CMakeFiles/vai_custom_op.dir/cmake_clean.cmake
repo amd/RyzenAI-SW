d84af5d2d7274b6236769435a83d0df27186e060
dachang <dachang@xilinx.com>
2023-11-28 06:21:03 -0700

    update onnx example

---
+++ tutorial/RyzenAI_quant_tutorial/onnx_example/vai_q_onnx/operators/custom_ops/build/CMakeFiles/vai_custom_op.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vai_custom_op.dir/src/bfp/cpu/bfp.cc.o"
+  "CMakeFiles/vai_custom_op.dir/src/bfp/cpu/bfp_kernel.cc.o"
+  "CMakeFiles/vai_custom_op.dir/src/bfp/cpu/nndct_cpu_math.cc.o"
+  "CMakeFiles/vai_custom_op.dir/src/bfp/cpu/nndct_fix_kernels_cpu.cc.o"
+  "CMakeFiles/vai_custom_op.dir/src/custom_op_bfp.cc.o"
+  "CMakeFiles/vai_custom_op.dir/src/custom_op_library.cc.o"
+  "CMakeFiles/vai_custom_op.dir/src/custom_op_qdq.cc.o"
+  "libvai_custom_op.pdb"
+  "libvai_custom_op.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vai_custom_op.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
