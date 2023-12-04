2371939bbd388a71be81c7b8b23597a8dd29aaa9
fanz-xlnx <56898996+fanz-xlnx@users.noreply.github.com>
2023-12-04 16:00:23 +0800

    Update camera.bat

--- tutorial/yolov8_e2e/implement/camera.bat
+++ tutorial/yolov8_e2e/implement/camera.bat
@@ -1,12 +1,13 @@
 set XLNX_VART_FIRMWARE=%cd%\..\1x4.xclbin
 set VITISAI_EP_JSON_CONFIG=%cd%\..\vaip_config.json
-set ENV_NAME=<YOUR-CONDA-ENV-NAME>
+:: please setup your conda env path below. For example, C:\Users\AMD\anaconda3\envs\ryzen_ai
+set ENV_PATH=<YOUR-CONDA-ENV-PATH>
 
-set PATH=%cd%\..\lib;C:\Users\AMD\anaconda3\envs\%ENV_NAME%;%PATH%;C:\Program Files\gflags\bin;C:\Program Files\glog\bin
+set PATH=%cd%\..\lib;%ENV_PATH%;%PATH%;C:\Program Files\gflags\bin;C:\Program Files\glog\bin
 
 ::the following two paths does not work in embedded python. To edit path for embbed python, change .pth file in the python folder
-set PATHONHOME=C:\Users\AMD\anaconda3\envs\%ENV_NAME%
-set PYTHONPATH=C:\Users\AMD\anaconda3\envs\%ENV_NAME%\Lib;C:\Users\AMD\anaconda3\envs\%ENV_NAME%\DLLs
+set PATHONHOME=%ENV_PATH%
+set PYTHONPATH=%ENV_PATH%\Lib;%ENV_PATH%\DLLs
 
 set NUM_OF_DPU_RUNNERS=4
 %cd%\..\bin\camera_yolov8_nx1x4  -c 5 -x 1 -y 1 -s 0 -D -R 1920x1080 -r 2560x1440 %1
