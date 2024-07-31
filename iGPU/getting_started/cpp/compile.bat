a4868d4ab234f36fa7ec1ba283cd45cc7d4bed71
savsrini <Savitha.Srinivasan@amd.com>
2024-07-31 14:44:10 -0700

    Make opencv dir configurable

--- iGPU/getting_started/cpp/compile.bat
+++ iGPU/getting_started/cpp/compile.bat
@@ -2,7 +2,14 @@
 
 if "%RYZEN_AI_INSTALLATION_PATH%" == "" echo RYZEN_AI_INSTALLATION_PATH not set. This script requires the RYZEN_AI_INSTALLATION_PATH env var to be set to the RyzenAI 1.2 installation folder. & goto :error
 
-cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DBUILD_SHARED_LIBS=OFF -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DCMAKE_CONFIGURATION_TYPES=Release -A x64 -T host=x64 -DCMAKE_INSTALL_PREFIX=. -DCMAKE_PREFIX_PATH=. -B build -S resnet50 -DOpenCV_DIR="C:/opencv/build" -G "Visual Studio 17 2022"
+REM Check if the first argument is provided
+if "%1"=="" (
+    echo Usage: %0 [OpenCV_DIR]
+    exit /b 1
+)
+
+
+cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DBUILD_SHARED_LIBS=OFF -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DCMAKE_CONFIGURATION_TYPES=Release -A x64 -T host=x64 -DCMAKE_INSTALL_PREFIX=. -DCMAKE_PREFIX_PATH=. -B build -S resnet50 -DOpenCV_DIR="%OpenCV_DIR%" -G "Visual Studio 17 2022"
 
 cmake --build .\build --config Release --target ALL_BUILD
 
